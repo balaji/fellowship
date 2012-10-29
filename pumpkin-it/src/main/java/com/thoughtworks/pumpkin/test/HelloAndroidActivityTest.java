cf708f69d9c92c6d9d166e1400a3e6d10c49c308
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-10-29 14:03:56 +0530

    Initial commit - with root android project and its functional test project

---
+++ pumpkin-it/src/main/java/com/thoughtworks/pumpkin/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.pumpkin.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.thoughtworks.pumpkin.HelloAndroidActivity;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super("com.thoughtworks.pumpkin", HelloAndroidActivity.class);
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
