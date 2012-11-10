7075de03198f003616aa4f6ae9a28b3f76a0f107
natchu <natchu@natchu.(none)>
2012-11-10 13:41:26 +0530

    added failure cases dialog box

--- pumpkin/src/main/java/com/thoughtworks/pumpkin/SigninActivity.java
+++ pumpkin/src/main/java/com/thoughtworks/pumpkin/SigninActivity.java
@@ -5,9 +5,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import com.thoughtworks.pumpkin.helper.Constant;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
+import com.thoughtworks.pumpkin.helper.ConnectionDetector;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 import javax.inject.Inject;
 
@@ -38,9 +42,34 @@ public class SigninActivity extends RoboActivity {
         setContentView(R.layout.signin);
         final SigninActivity signinActivity = this;
 
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+        // Setting Dialog Title
+        alertDialog.setTitle("Network Settings");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("Connection Not Established");
+
+        // Setting OK Button
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+
+
+        final ConnectionDetector cd = new ConnectionDetector(getApplicationContext());
+
         twitterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Boolean isInternetPresent = cd.isConnectingToInternet();
+                if(!isInternetPresent)
+                {
+                    // Showing Alert Message
+                    alertDialog.show();
+                }
+                else
                 startActivity(new Intent(signinActivity, TwitterLoginActivity.class));
             }
         });
@@ -48,6 +77,13 @@ public class SigninActivity extends RoboActivity {
         facebookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Boolean isInternetPresent = cd.isConnectingToInternet();
+                if(!isInternetPresent)
+                {
+                    // Showing Alert Message
+                    alertDialog.show();
+                }
+                else
                 startActivity(new Intent(signinActivity, FacebookLoginActivity.class));
             }
         });
@@ -55,6 +91,13 @@ public class SigninActivity extends RoboActivity {
         googleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Boolean isInternetPresent = cd.isConnectingToInternet();
+                if(!isInternetPresent)
+                {
+                    // Showing Alert Message
+                    alertDialog.show();
+                }
+                else
                 startActivity(new Intent(signinActivity, GoogleLoginActivity.class));
             }
         });
@@ -62,6 +105,13 @@ public class SigninActivity extends RoboActivity {
         yahooButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Boolean isInternetPresent = cd.isConnectingToInternet();
+                if(!isInternetPresent)
+                {
+                    // Showing Alert Message
+                    alertDialog.show();
+                }
+                else
                 startActivity(new Intent(signinActivity, YahooLoginActivity.class));
             }
         });
---
+++ pumpkin/src/main/java/com/thoughtworks/pumpkin/helper/ConnectionDetector.java
@@ -0,0 +1,56 @@
+package com.thoughtworks.pumpkin.helper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: natchu
+ * Date: 11/10/12
+ * Time: 12:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+public class ConnectionDetector {
+
+    private Context context;
+
+    public ConnectionDetector(Context context){
+        this.context = context;
+    }
+
+    public boolean isConnectingToInternet(){
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null)
+        {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+
+  /*  {
+        ConnectionDetector cd = new ConnectionDetector(getApplicationContext());
+
+        Boolean isInternetPresent = cd.isConnectingToInternet(); // true or false
+
+        if (isInternetPresent) {
+            // Internet Connection is Present
+            // make HTTP requests
+            showAlertDialog(AndroidDetectInternetConnectionActivity.this, "Internet Connection",
+                    "You have internet connection", true);
+        } else {
+            // Internet connection is not present
+            // Ask user to connect to Internet
+            showAlertDialog(AndroidDetectInternetConnectionActivity.this, "No Internet Connection",
+                    "You don't have internet connection.", false);
+    }*/
+}
