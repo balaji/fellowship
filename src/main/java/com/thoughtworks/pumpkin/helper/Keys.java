70005f28096c61b0b053d5262bf07bf2a08d16a6
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-12-05 16:07:54 +0530

    removed tests project, not used at all

---
+++ src/main/java/com/thoughtworks/pumpkin/helper/Keys.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.pumpkin.helper;
+
+public interface Keys {
+    String GOOGLE_CONSUMER_KEY = "200520456264-jiej9o9def6ep4veudahfcs8sb0p8kg2.apps.googleusercontent.com";
+    String YAHOO_CONSUMER_KEY = "dj0yJmk9SXpNNTdtOEltSUZsJmQ9WVdrOVlWSnNkbmd6TlRJbWNHbzlNakExTURNek1UYzJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD05Nw--";
+    String YAHOO_CONSUMER_SECRET = "e9c760aa447eabb1eed18c24bb06f2e91bc9239e";
+    String GOOGLE_CONSUMER_SECRET = "kHRTdXE4ct4hb_tn3UqyqQDr";
+    String TWITTER_CONSUMER_KEY = "wROkHKann3dWrY2FzofvQ";
+    String TWITTER_CONSUMER_SECRET = "m9i27lNukzKFU5gcQ0ZNrOhSK3WhYa8pM1Gr2hWZiY";
+    String GOOGLE_API_KEY = "AIzaSyAkwxYZuP0W29EOxUWKST6SZipjs_CeTfI";
+    String PARSE_API_KEY = "kYtvbqP7IF3YVrlemJAPYXIeSvV1C4yj6VekYQFe";
+    String PARSE_JAVASCRIPT_KEY = "FIh138E1ICzEPpSruYN6tdYsnpPNQcXj7EKvgOXe";
+    String PARSE_CLIENT_KEY = "kAGldS8iG5zmMQIl5HHQbPwPeIoqrb2Id6ONMl1J";
+    String FACEBOOK_APP_ID = "509095802449138";
+}
