b191b2d7a7ad7c0cfde290a5d02ea67a1de603f4
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-12-13 15:53:33 +0530

    Added a dummy page and reverted side panel colors

--- src/main/java/com/thoughtworks/pumpkin/adapter/SidePanelAdapter.java
+++ src/main/java/com/thoughtworks/pumpkin/adapter/SidePanelAdapter.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.pumpkin.adapter;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SimpleExpandableListAdapter;
@@ -24,16 +23,6 @@ public class SidePanelAdapter extends SimpleExpandableListAdapter {
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         TextView childView = (TextView) super.getChildView(groupPosition, childPosition, isLastChild, convertView, parent);
         childView.setTextAppearance(sidePanel.getActivity(), android.R.style.TextAppearance_Medium);
-        childView.setTextColor(Color.WHITE);
-        childView.setBackgroundColor(Color.GRAY);
         return childView;
-    }
-
-    @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        TextView groupView = (TextView) super.getGroupView(groupPosition, isExpanded, convertView, parent);
-        groupView.setTextColor(Color.WHITE);
-        groupView.setBackgroundColor(Color.DKGRAY);
-        return groupView;
     }
 }
