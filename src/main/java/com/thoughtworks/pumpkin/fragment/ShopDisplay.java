d4a18b13064b84161219f927077c75066bc8ca15
Balaji Damodaran <damodaran.balaji@gmail.com>
2012-12-28 10:06:43 +0530

    added 'marker' to maps

--- src/main/java/com/thoughtworks/pumpkin/fragment/ShopDisplay.java
+++ src/main/java/com/thoughtworks/pumpkin/fragment/ShopDisplay.java
@@ -1,5 +1,9 @@
 package com.thoughtworks.pumpkin.fragment;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.LayoutInflater;
@@ -7,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 import com.slidingmenu.lib.SlidingMenu;
 import com.thoughtworks.pumpkin.BaseActivity;
 import com.thoughtworks.pumpkin.R;
@@ -21,11 +26,17 @@ import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
 import roboguice.fragment.RoboFragment;
 
 import java.io.File;
+import java.util.ArrayList;
 
-public class ShopDisplay extends RoboFragment {
+public class ShopDisplay extends RoboFragment implements LocationListener {
+
+    MapView mapView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,7 +45,7 @@ public class ShopDisplay extends RoboFragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        ((BaseActivity)getActivity()).getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+        ((BaseActivity) getActivity()).getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
         RelativeLayout layout = (RelativeLayout) view.findViewById(R.id.mapLayout);
         XYTileSource mbtilesSource = new XYTileSource("mbtiles", ResourceProxy.string.offline_mode, 19, 21, 256, ".png", "http://pumpkin.thoughtworks.com/");
         DefaultResourceProxyImpl resourceProxy = new DefaultResourceProxyImpl(getActivity().getApplicationContext());
@@ -44,7 +55,7 @@ public class ShopDisplay extends RoboFragment {
         MapTileModuleProviderBase moduleProvider = new MapTileFileArchiveProvider(simpleReceiver, mbtilesSource, files);
         MapTileProviderArray tileProviderArray = new MapTileProviderArray(mbtilesSource, null, new MapTileModuleProviderBase[]{moduleProvider});
 
-        MapView mapView = new MapView(getActivity(), 256, resourceProxy, tileProviderArray);
+        mapView = new MapView(getActivity(), 256, resourceProxy, tileProviderArray);
         mapView.setLayoutParams(new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT
@@ -56,6 +67,52 @@ public class ShopDisplay extends RoboFragment {
         mapView.setClickable(true);
         mapView.setUseDataConnection(false);
         mapView.getController().setZoom(19);
-        mapView.getController().setCenter(new GeoPoint(12.9840, 80.246));
+        GeoPoint point = new GeoPoint(12.9840, 80.246);
+        mapView.getController().setCenter(point);
+
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 100, this);
+
+        ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
+        items.add(new OverlayItem("Here", "SampleDescription", point));
+
+        ItemizedOverlay<OverlayItem> locationOverlay = new ItemizedIconOverlay<OverlayItem>(items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
+                        Toast.makeText(getActivity(), "Item '" + item.mTitle, Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onItemLongPress(final int index, final OverlayItem item) {
+                        Toast.makeText(getActivity(), "Item '" + item.mTitle, Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+                }, resourceProxy);
+        mapView.getOverlays().add(locationOverlay);
+        mapView.invalidate();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude() * 1E6);
+        int lng = (int) (location.getLongitude() * 1E6);
+        GeoPoint gpt = new GeoPoint(lat, lng);
+        mapView.getController().setCenter(gpt);
+        mapView.invalidate();
+    }
+
+    @Override
+    public void onProviderDisabled(String arg0) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
     }
 }
